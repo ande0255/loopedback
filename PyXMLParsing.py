70ae53bb1d146ac993a458a9e35fa2c34e43ce40
loopedback <ande0255@gmail.com>
2020-07-11 19:11:39 -0500

    Initial commit

---
+++ PyXMLParsing.py
@@ -0,0 +1,12 @@
+import xml.etree.ElementTree as ET
+
+stream = open('AwesomeFood.xk', 'r') # This opens the XML File 'AwesomeFood' I created for parsing
+
+xml = ET.parse(stream) # This tell the script to Parse data into an Element Tree Object
+
+root = xml.getroot() # This gets the "Root" Element of each Element Tree (The Parent!)
+
+for e in root:
+    print(ET.tostring(e))
+    print(**)
+    print(e.get(*Id*))
