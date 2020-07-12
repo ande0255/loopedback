20149936b202177575cfb08becfb47daf38f6dd4
loopedback <ande0255@gmail.com>
2020-07-12 16:12:29 -0500

    Commit comment

--- PyJSONParsing.py
+++ PyJSONParsing.py
@@ -1,1 +1,16 @@
-something
+import json 
+  
+# Opening JSON file 
+f = open('LoopedBackEmployees.json',) 
+  
+# returns JSON object as  
+# a dictionary 
+data = json.load(f) 
+  
+# Iterating through the json 
+# list 
+for i in data['LoopedBack_Employees']: 
+    print(json.dumps(data, indent=1))
+  
+# Closing file 
+f.close() 
