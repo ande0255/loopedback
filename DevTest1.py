3e089557349bb5873b29ef28cd4204148ecd12a0
loopedback <ande0255@gmail.com>
2020-07-28 20:33:13 -0500

    Starting Python MasterClass!

--- DevTest1.py
+++ DevTest1.py
@@ -1,5 +1,9 @@
-dogs = ['Chihaha', 'Bulldog', 'Poopy']
+greeting = "Hello"
+name = input("Please type your name here ")
+print (greeting + ' ' + name)
 
-for dog in dogs:
-    print(dog)
-print(" ")
+splitString = "This is a \nstring that is being \nsplit into multiple lines!"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
