4bfa37f16f563b41a145f23c0dca5611e200cf09
Loopy <Loopy@Loopedback.com>
2020-07-30 21:33:18 -0500

    RESTConf Demo from Hank Vid

--- CONF.py
+++ CONF.py
@@ -1,1 +1,12 @@
-something
+from device_info import ios_xe1
+from ncclient import manager
+
+if __name__ == '__man__':
+    with manager.connect(host=ios_xe1["address], port=ios_xe1"],
+                        username=ios_xe1["username"],
+                        password=ios_xe1["password"],
+                        hostkey_verify=False) as m:
+
+print("Here are the NETCONF Capabilities")
+for capibility in m.server_capabilities:
+    print(capability)
