ae242b3f31dc9ddb63fecf484be6cac953eb5746
Loopy <Loopy@Loopedback.com>
2020-08-22 19:29:25 -0500

    Commit All Current changes

---
+++ escape.py
@@ -0,0 +1,18 @@
+splitString = "This string is\ngoing to be split\ninto three lines!"
+print(splitString)
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('I am absolutely sure I\'ll definitely nail\'s this demo the first time!\'')
+print('Loopy said "I\'ll definitely pass the DevNet\'s Exams every try on my first try!\"')
+print("""Loopy said "I bet I will get a "1000/1000" score \
+ on my next Cisco Exam!" """)
+anotherLineSplit = """This is another \
+line that is broken \
+up into 5 lines \
+this time to see \
+if it will work!"""
+print(anotherLineSplit)
+
+print("C:\\Users\\testUser\\noodles.txt")
+print(r"C:\Users\testUser\noodles.txt")
