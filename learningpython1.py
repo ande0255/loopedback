c6f9333c222a7067f84b0fb3402f21cd279d2c1b
Loopy <Loopy@Loopedback.com>
2020-08-23 20:17:17 -0500

    Commit all

--- learningpython1.py
+++ learningpython1.py
@@ -1,1 +1,1 @@
-#learning python from scratch!
+print('Hello World!', 3 * 5, """MoreQuotes!""", '4 + 5')
---
+++ strings.py
@@ -0,0 +1,18 @@
+greeting = "Hello"
+print(greeting)
+
+somefloat = 4.555
+print(somefloat)
+
+myname = "Loopy"
+print(myname)
+
+somenumber = 45
+print(somenumber)
+
+print(type(greeting))
+print(type(somefloat))
+print(type(myname))
+print(type(somenumber))
+
+print(greeting + " my name is " + myname + " and I am " + str(somenumber) + " years old!")
