fbb0bc96efd90ae745deda7bd560984dd6196559
Loopy <Loopy@Loopedback.com>
2020-08-26 21:37:14 -0500

    First wave of integer math

--- operators.py
+++ operators.py
@@ -1,1 +1,11 @@
-operators.py
+#Python Math Operators / Numeric Data Types
+
+a = 16
+b = 4
+
+print(a + b)   #integer addition
+print(a - b)   #integer substraction
+print(a * b)   #integer multiplcation
+print(a / b)   #integer division
+print (a // b) #integer (floored) division that rounds down
+print(a % b)   #the remainder after integer division
