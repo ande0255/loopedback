a8362d658e76a39cd356e89d35c0f1e3c4c4ad45
Loopy <Loopy@Loopedback.com>
2020-08-29 02:17:36 -0500

    Init strings2 commit and operators

--- operators.py
+++ operators.py
@@ -2,10 +2,21 @@
 
 a = 16
 b = 4
+c = a + b
+d = c / 6
+e = d - 112
 
-print(a + b)   #integer addition
-print(a - b)   #integer substraction
-print(a * b)   #integer multiplcation
-print(a / b)   #integer division
-print (a // b) #integer (floored) division that rounds down
-print(a % b)   #the remainder after integer division
+# PEMDAS = Parenthesis / Exponents / Multiplication / Division / Addition / Subtraction
+
+print(((a + b) /6) - (8 * 14))
+
+print(a / (b * a) / b)
+
+
+
+# print(a + b)   #integer addition
+# print(a - b)   #integer substraction
+# print(a * b)   #integer multiplcation
+# print(a / b)   #integer division
+# print (a // b) #integer (floored) division that rounds down
+# print(a % b)   #the remainder after integer division
