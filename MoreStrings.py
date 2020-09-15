353b185f30d9b3baf73ab6f935e43d19a3acf367
Loopy <Loopy@Loopedback.com>
2020-09-15 18:44:12 -0500

    Splice Step Backwards

--- MoreStrings.py
+++ MoreStrings.py
@@ -1,6 +1,7 @@
-Laborday = "Holiday"
+# Slicing Steps Backwards!
 
-#     01234567
-print(Laborday)
+letters = "abcdefghijklmnopqrstuvqxyz"  # Yes I just sang the ABCs song to get that correct :)
 
-print(Laborday[5 - 7])
+backwards = letters[25:0:-1]
+
+print(backwards)
