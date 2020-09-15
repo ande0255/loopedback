1f442a9da3d321cdd1b7baf9d6076447f73326c5
Loopy <Loopy@Loopedback.com>
2020-09-15 18:41:47 -0500

    Added awesome demo

--- strings2.py
+++ strings2.py
@@ -1,1 +1,11 @@
-strings2.py
+ 
+#Getting deeper into it!
+
+number = "7,230;403 826.271:112,165"
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val)for val in values])
+
+#Kapow! I have no idea what I just coded but lets see what happens!
