5dcbfe2e57094d7013733420d7cb84183ce46292
Loopy <Loopy@Loopedback.com>
2020-09-22 16:28:19 -0500

    commit

--- anotherfile.py
+++ anotherfile.py
@@ -1,3 +1,5 @@
 age = 35
 
-print("My age is ", + str(age) + "years old!")
+print("My age is " + str(age) + " years old!")
+
+print("My age is {0} years old!".format(age))
--- sequences.py
+++ sequences.py
@@ -1,5 +1,18 @@
-str1 = "This"
-str2 = "is"
-str3 = "going to be"
-str4 = "Loopy's"
-str5 = "demo"
+str1 = "This "
+str2 = "is "
+str3 = "going to be "
+str4 = "Loopy's "
+str5 = "demo!"
+
+print(str1 + str2 + str3 + str4 + str5)
+
+print("Hello! " * 5)
+print("Hello! " * (5 + 4))  # This will perform the math function first (PEMDAS)
+print("Hello! " * 5 + str(4))
+
+#today = "Monday"
+
+#print("day" in today)   #true
+#print("Mon" in today)   #true
+#print("Friday!!!" in today) #false
+#print("Hello!" in today)    #false
